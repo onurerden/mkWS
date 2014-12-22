135fc06c7b8286f1e65e1f3af17bbc8179c8078b
oerden <oerden@oerden-lp-22.belbim.local>
2014-12-22 11:17:30 +0200

    NEW

--- src/java/mkws/IDeviceServer.java
+++ src/java/mkws/IDeviceServer.java
@@ -13,22 +13,27 @@ public interface IDeviceServer {
 
     int touchServer(String uid, String deviceType);
     //uid cihaz tekil numarasi maks 45 hane, deviceType mk (mikrokopter) ya da mp (mobilephone)
-    //-2 mikrokopter kayd? yok, -3 mobile phone kayd? yok, -1 DB error
+    //-2 mikrokopter kaydı yok, -3 mobile phone kaydı yok, -1 DB error
 
     int registerDevice(String uid, String name, String deviceType);
     //uid cihaz tekil numarasi maks 45 hane, name cihaz ismi
     //deviceType mk (mikrokopter) ya da mp (mobilephone)
-    //-1 ise db erişim hatası. -2 ya da -3 ise kayd? yok ancak yine de kay?t edilemedi.
+    //-1 ise db erişim hatası. -2 ya da -3 ise kaydı yok ancak yine de kayıt edilemedi.
 
-    int sendStatus(String jsonStatus); 
-    //gelecek olan json KopterStatus s?n?f?ndaki gibi olmal?d?r.
+    int sendStatus(String jsonStatus);
+    //gelecek olan json KopterStatus sınıfındaki gibi olmalıdır.
     //dönüş 1 ise başarılı işlem gerçekleştimiştir; dönüş 0 ise data kaydedilmemiştir.
-    //dönüş -1 ise db ba?lantısında sıkıntı bulunmaktad?r.
+    //dönüş -1 ise db bağlantısında sıkıntı bulunmaktadır.
 
     String getTask(int deviceID);
     //yalnizca mk tarafindan kullanilir. koptere iletilecek yeni bir 
     //gorev varsa json formatinda cevap donulur.
     //-2 gelirse söz konusu kopter ID için bir konum bilgisi yoktur.
     //-1 db connection Error
+
+    int sendFollowMeData(String json);
+    //Yalnızca followMeDevice tarafından kullanılır.
+    //FollowMeData sınıfındaki objeyi json olarak gonderir.
+    //Başarılı gönderim halinde 0, hatalı json -2, db erişim hatası -1
 
 }
